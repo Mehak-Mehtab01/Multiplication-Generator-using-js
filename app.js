f2b5d558ee97c1aade9824a50f874f9d86555acc
MEHAK MEHTAB <mehakmehtab01@gmail.com>
2024-12-24 10:25:55 +0500

    app.js

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-document.getElementById("generateBtn").addEventListener("click", function() {
-    const num = document.getElementById("numInput").value;
+document.getElementById("generateBtn").addEventListener("click", function () {
+    const numInput = document.getElementById("numInput");
+    const num = numInput.value; 
     const resultContainer = document.getElementById("tableResult");
-    resultContainer.innerHTML = "";
+    resultContainer.innerHTML = ""; 
+
     if (num === "" || isNaN(num)) {
         resultContainer.innerHTML = "<p>Please enter a valid number</p>";
         resultContainer.style.color = "red";
-        resultContainer.style.marginLeft= "25px"
+        resultContainer.style.marginLeft = "25px";
         return;
     }
-   for (let i = 1; i <= 10; i++) {
+
+    for (let i = 1; i <= 10; i++) {
         resultContainer.innerHTML += `<p>${num} x ${i} = ${num * i}</p>`;
-        resultContainer.style.color="black";
-         resultContainer.style.marginLeft= "65px";
-         resultContainer.style.fontWeight="bold";
+        resultContainer.style.color = "black";
+        resultContainer.style.marginLeft = "65px";
+        resultContainer.style.fontWeight = "bold";
     }
-    num.value=" ";
+
+   
+    numInput.value = "";
 });
